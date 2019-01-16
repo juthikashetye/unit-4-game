8d170425ae8daa8dc53331dc785fdd0439033aec
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-01-16 10:59:32 -0800

    Assigned unique crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ var myScoreCounter = 0;
 var winCounter = 0;
 var lossCounter = 0;
 
+// Set text of the counters
 function displayCountersOnScreen() {
   $("#goalnumber").html(goal);
   $("#score").html(myScoreCounter);
@@ -12,15 +13,27 @@ function displayCountersOnScreen() {
 
 function assignCrystalValue() {
   var arrayOfJewels = [$('#blue'), $('#green'), $('#red'), $('#yellow')];
+  var arrayOfCrystalValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  // Formula for shuffling array of crystalValues
+  var crystalValue = arrayOfCrystalValues.sort(function(a, b) {
+    return 0.5 - Math.random()
+  });
+
+  // Iterate through the jewels and assign unique crystal value
+  // to each jewel according to 
+  // the shuffled arrayOfCrystalValues' indexes
   for (var i = 0; i < arrayOfJewels.length; i++) {
-    var crystalValue = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
-    arrayOfJewels[i].attr("data-crystalvalue", crystalValue);
+
+    arrayOfJewels[i].attr("data-crystalvalue", crystalValue[i]);
+
   }
 }
 
 $(".jewelImage").on("click", function() {
   var clickedJewelValue = ($(this).attr("data-crystalvalue"));
   clickedJewelValue = parseInt(clickedJewelValue);
+  // Add the values of data-crystalvalues of clicked jewels
+  // to your score
   myScoreCounter += clickedJewelValue;
   $("#score").html(myScoreCounter);
 
