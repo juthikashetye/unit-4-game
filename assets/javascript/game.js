5720cdda295bc28bb10047a9712239c7aaa0f1ce
Juthika Shetye <juthika.bhonsle@gmail.com>
2019-01-19 20:00:19 -0800

    Modified variable names

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,7 +16,7 @@ function assignCrystalValue() {
   var arrayOfCrystalValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   // Formula for shuffling array of crystalValues
   var crystalValue = arrayOfCrystalValues.sort(function(a, b) {
-    return 0.5 - Math.random()
+    return 0.5 - Math.random();
   });
 
   // Iterate through the jewels and assign unique crystal value
@@ -24,14 +24,13 @@ function assignCrystalValue() {
   // the shuffled arrayOfCrystalValues' first 4 indexes
   for (var i = 0; i < arrayOfJewels.length; i++) {
 
-    arrayOfJewels[i].attr("data-crystalvalue", crystalValue[i]);
+    arrayOfJewels[i].data("crystalvalue", crystalValue[i]);
 
   }
 }
 
 $(".jewelImage").on("click", function() {
-  var clickedJewelValue = ($(this).attr("data-crystalvalue"));
-  clickedJewelValue = parseInt(clickedJewelValue);
+  var clickedJewelValue = parseInt($(this).data("crystalvalue"));
   // Add the values of data-crystalvalues of clicked jewels
   // to your score
   myScoreCounter += clickedJewelValue;
@@ -51,7 +50,9 @@ $(".jewelImage").on("click", function() {
 });
 
 function start() {
-  goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  var upperBound = 120;
+  var lowerBound = 19;
+  goal = Math.floor(Math.random() * (upperBound-lowerBound+1)) + lowerBound;
   myScoreCounter = 0;
   assignCrystalValue();
   displayCountersOnScreen();
